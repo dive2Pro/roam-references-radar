4335ce1d4e098182a08e8631603be225cb1c32a7
黄艺川 <toylucklebo@gmail.com>
2025-07-18 09:33:47 +0800

    feat: ui css

--- src/comps/BlockKeyword.tsx
+++ src/comps/BlockKeyword.tsx
@@ -87,7 +87,7 @@ export function BlockKeyword({
       }
       captureDismiss
     >
-      <span className="block-keyword">{data.text}</span>
+      <span className="radar-highlighter">{data.text}</span>
     </Popover>
   );
 }
